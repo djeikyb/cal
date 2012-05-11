68a2209157d279620a5380e74e758fea7efeadb6
Jacob Certain <jacob.certain@gilbertschools.net>
2012-05-10 18:51:59 -0700

    sql file to insert test data

---
+++ testdata.sql
@@ -0,0 +1,4 @@
+insert into events VALUES ('', '0000-00-00', '',      '',      'other',  'beginning of common era', '');
+insert into events VALUES ('', '2012-12-31', '21:00', '',      'other',   'new year\'s eve party',   '');
+insert into events VALUES ('', '2012-05-05', '15:00', '18:00', 'other',   'cinco de mayo happy hour', '');
+insert into events VALUES ('', '2012-05-05', '',      '',      'holiday', 'cinco de mayo', '');
