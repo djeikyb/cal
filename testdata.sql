96ab127182c396277f9e569507281667abb82e46
Jacob Certain <jacob.certain@gilbertschools.net>
2012-05-21 15:04:14 -0700

    elongated halloween description

--- testdata.sql
+++ testdata.sql
@@ -1,5 +1,5 @@
 insert into events VALUES ('', '',            '',      '',       '',         '', '');
-insert into events VALUES ('', '2012-10-31',  '',      '',       'holiday',  'halloween', '');
+insert into events VALUES ('', '2012-10-31',  '',      '',       'holiday',  'all hallows eve, when ghouls moan and banshees scream and children become the monsters under their beds', '');
 insert into events VALUES ('', '0000-00-00',  '',      '',       'other',    'beginning of common era', '');
 insert into events VALUES ('', '2012-12-31',  '21:00', '',       'other',    'new year\'s eve party',   '');
 insert into events VALUES ('', '2012-05-05',  '15:00', '18:00',  'other',    'cinco de mayo happy hour', '');
