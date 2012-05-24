101a3636a3676bd478434efed52ad00749bdfb94
Jacob Certain <jacob.certain@gilbertschools.net>
2012-05-23 17:18:43 -0700

    viewMonth(), viewDay(), and viewEvent() all work
    Aside from getting those methods to a working state, also tightened up
    wording on menu options, added comments, and decomposed methods

--- Tui.java
+++ Tui.java
@@ -1,5 +1,10 @@
 package gps.tasks.task3663;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.joda.time.LocalDate;
@@ -22,13 +27,14 @@ public class Tui
   {
     StringBuilder sb = new StringBuilder();
 
-    sb.append("q) Enter 'q' to quit.\n");
+    sb.append("q) Quit\n");
     sb.append("\n> ");
 
     return sb.toString();
   }
 
   /**
+   *  Returns menu header.
    */
   public String menuHead()
   {
@@ -64,7 +70,7 @@ public class Tui
    *  Read "^[:digit:]*"
    *
    *  @param s  String that begins with digits
-   *  @return   Integer created from first sequence of digits
+   *  @return   Integer created from first digit sequence
    */
   public Integer readInteger(String s)
   {
@@ -72,14 +78,27 @@ public class Tui
     for (Character c : s.toCharArray())
     {
       if (Character.isDigit(c))
-      {
         digits.append(c);
-      }
-      else break;
+      else
+        break;
     }
 
     return Integer.valueOf(digits.toString());
   }
+
+  /**
+   *  Judge intent by first character. Helps evaluate user input.
+   *
+   *  @return first character, or 'z';
+   */
+  public Character getIntent(String input)
+  {
+    if (input.length() != 0)
+      return input.charAt(0);
+    else
+      return 'z';
+  }
+
 
 
 //------------------------------------------------------------------------------
@@ -98,46 +117,44 @@ public class Tui
   {
     clearScreen();
 
+    // set now to beginning of month
+    now = new LocalDate(now.year().get(), now.monthOfYear().get(), 1);
+
+    // when are we?
     System.out.println(cal.month(now) + "\n");
 
-
     // navigation
-    System.out.println("p) Previous month.");
-    System.out.println("n) Next month.");
+    System.out.println("p) Previous month");
+    System.out.println("n) Next month");
 
     // command
-    System.out.println("#) View day #.");
+    System.out.println("#) View day #");
 
     // footer
     System.out.print(menuFoot());
 
     // input
     String input = getInput();
+    Character intent = getIntent(input);
 
-    // judge intent by first character
-    Character intent;
-    if (input.length() != 0)
-    {
-      intent = input.charAt(0);
-    }
-    else intent = 'z';
-
-    if (Character.isDigit(intent))  // if first char == digit, intent is command
+    if (Character.isDigit(intent))  // if digit, intent is viewDay()
     {
       Integer n = readInteger(input);
 
       // Call viewDay() with $now incremented by the number we claim the user
       // intended. Handy, cause it avoids error handling by avoiding an error.
       // User *may* not expect it, though nothing is destroyed, and getting
-      // back is relatively easy.
+      // back is relatively easy. Even better, might grow to be considered a
+      // "power-user" feature!
       viewDay(new LocalDate(now.plusDays(n - 1)));
     }
-    else  // else if first char != digit, intent is navigation
+    else  // else if != digit, intent is navigation
     {
       switch (intent)
       {
         case 'p': viewMonth(now.minusMonths(1));  break;
         case 'n': viewMonth(now.plusMonths(1));   break;
+        case 'q': System.exit(0);                 break;
         default:  viewMonth(now);                 break;  // ultimate else
       }
     }
@@ -145,21 +162,122 @@ public class Tui
   }
 
   /**
-   *  Views a day and its events. Leads to view event. Indirectly leads to edit
-   *  event. Also leads to [view week XOR view month]
+   *  Views a day and its events.
+   *
+   *  Leads to view event. Indirectly leads to edit event. Also leads to
+   *  [view week XOR view month]
    */
   public void viewDay(LocalDate now)
   {
     clearScreen();
 
+    // when are we?
+    System.out.println(now);
+
+    // get the day's events, if any
+    List<Integer> eids = new ArrayList<Integer>();
+    try
+    {
+      eids = CalendarRegistry.getEventsFor(now);
+    }
+    catch (SQLException e)
+    {
+      System.err.println("database error");
+    }
+
+    // if no events, say so, otherwise make options
+    Map<Integer, Integer> options = new HashMap<Integer, Integer>();
+    if (eids.isEmpty())
+    {
+      System.out.println("No events this day.");
+    }
+    else
+    {
+      // get taglines
+      Map<Integer, String> taglines = new HashMap<Integer, String>();
+      try
+      {
+        taglines = CalendarRegistry.getEventTaglines(eids);
+      }
+      catch (SQLException e)
+      {
+        System.err.println("database error");
+      }
+
+      // make options
+      System.out.printf("\nToday's events:\n\n");
+      Integer counter = 0;
+      for (Integer id : eids)
+      {
+        counter++;
+        options.put(counter, id);
+        System.out.printf("%s. %s\n", counter, taglines.get(id));
+      }
+      System.out.println();
+    }
+
     // navigation
-    System.out.println("b) Back to [month|week].");
-    System.out.println("p) Previous day.");
-    System.out.println("n) Next day.");
+    System.out.println("b) Back to month");
+    System.out.println("p) Previous day");
+    System.out.println("n) Next day");
 
     // command
-    System.out.println("#) View event #.");
-    System.out.println("a) Add new event.");
+    System.out.println("#) View event #");
+    System.out.println("a) Add new event");
+
+    // footer
+    System.out.print(menuFoot());
+
+    // input
+    String input = getInput();
+    Character intent = getIntent(input);
+
+    if (Character.isDigit(intent))  // if digit, intent is viewEvent
+    {
+      Integer n = readInteger(input);
+
+      // ignore command if the option doesn't exist
+      if (options.containsKey(n))   viewEvent(now, options.get(n));
+      else                          viewDay(now);
+    }
+    else
+    {
+      switch (intent)
+      {
+        case 'a': editEvent(-1);              break;
+
+        // navigation
+        case 'b': viewMonth(now);             break;
+        case 'p': viewDay(now.minusDays(1));  break;
+        case 'n': viewDay(now.plusDays(1));   break;
+        case 'q': System.exit(0);             break;
+        default:  viewDay(now);               break;  // ultimate else
+      }
+    }
+
+  }
+
+  /**
+   *  Views an event.
+   *
+   *  Leads to edit event, attach guests, and view day.
+   */
+  public void viewEvent(LocalDate now, Integer eid)
+  {
+    clearScreen();
+
+    // when are we?
+    System.out.println(now);
+
+    // navigation
+    System.out.println("b) Back to day");
+    System.out.println("p) Previous event");
+    System.out.println("n) Next event");
+
+    // command
+    System.out.println("d) Delete this event");
+    System.out.println("e) Edit this event");
+    System.out.println("a) Attach guests");
 
     // footer
     System.out.println(menuFoot());
@@ -169,28 +287,9 @@ public class Tui
   }
 
   /**
-   *  Views an event. Leads to edit event, attach guests, and view day.
-   */
-  public void viewEvent()
-  {
-    // navigation
-    System.out.println("b) Back to day.");
-    System.out.println("h) Previous event.");
-    System.out.println("l) Next event.");
-
-    // command
-    System.out.println("d) Delete this event.");
-    System.out.println("e) Edit this event.");
-    System.out.println("a) Attach guests.");
-
-    // footer
-    System.out.println(menuFoot());
-  }
-
-  /**
    *  Edits an event. Pushes to view event.
    */
-  public void editEvent()
+  public void editEvent(Integer eid)
   {
     /*  should show old info while prompting for new, if any exists
      *  should keep old info if blank line
@@ -212,9 +311,9 @@ public class Tui
   public void attachGuest()
   {
     // navigation
-    System.out.println("b) Back to event.");
+    System.out.println("b) Back to event");
 
     // command
-    System.out.println("#) Select guest #.");
+    System.out.println("#) Select guest #");
   }
 }
