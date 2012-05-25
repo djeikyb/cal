f156a0b67f5362a1ffa7fb1a31c0434bc144eacb
Jacob Certain <jacob.certain@gilbertschools.net>
2012-05-25 15:05:11 -0700

    test now tests for and solves the problem intended

--- Test.java
+++ Test.java
@@ -7,8 +7,24 @@ public class Test
   
   public static void main(String[] args)
   {
-    ArrayList<String> foo = new ArrayList<String>();
     
+    String[] s = String.format("").split(",");
+    
+    System.out.println("s: " + s + "|");
+    
+    ArrayList<String> foo = new ArrayList<String>();
+    System.out.println("size of foo: " + foo.size());
+    
+    for (String o : s)
+    {
+      System.out.println(o + "|");
+      
+      if (!o.isEmpty()) foo.add(o);
+      
+      System.out.println(foo);
+    }
+    
+    System.out.println("size of foo: " + foo.size());
     System.out.println("foo: " + foo);
     
     foo.add("huzzah");
@@ -19,6 +35,7 @@ public class Test
         1, foo.toString().length() - 1);
     
     System.out.println("bar: " + bar);
+    
   }
 
 }
